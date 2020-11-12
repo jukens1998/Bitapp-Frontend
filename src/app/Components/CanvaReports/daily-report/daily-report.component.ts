1ba2fd22209a081f00d4e07f9785ae7bc359dc7c
Juan Diego Tovaria <juken.555@hotmail.com>
2020-11-12 16:50:01 -0500

    Cambios de colores aplicados

--- src/app/Components/CanvaReports/daily-report/daily-report.component.ts
+++ src/app/Components/CanvaReports/daily-report/daily-report.component.ts
@@ -10,38 +10,50 @@ import * as CanvasJS from '../../../../assets/canvasjs/canvasjs.min';
   styleUrls: ['./daily-report.component.css']
 })
 export class DailyReportComponent implements OnInit {
-// array that contains the data that will be render on the canvas
-public projectsDataReport: any[];
+  // array that contains the data that will be render on the canvas
+  public projectsDataReport: any[];
 
-constructor(private reports: ReportsService, private binacle:BinacleService) { }
+  constructor(private reports: ReportsService, private binacle: BinacleService) { }
   ngOnInit() {
-
-
+    // Render the main canvas
     this.canvas();
   }
   public canvas() {
     this.binacle.getTasks().subscribe((result: any[]) => {
+      // Data from porjects
       this.projectsDataReport = result;
-      let chart = new CanvasJS.Chart('chartContainer1', {
-        theme: 'light2',
-        animationEnabled: true,
-        exportEnabled: true,
-        title: {
-          text: 'Reporte laboral',
-        },
-        data: [{
-          type: 'pie',
-          showInLegend: true,
-          toolTipContent: '<b>{name}</b>: {y} (#percent%)',
-          indexLabel: '{name} - #percent%',
-          dataPoints: [
-          ],
-        }],
-      });
+
+      CanvasJS.addColorSet("greenShades",
+        [//colorSet Array
+          "#0F1C37",
+          "#293064",
+          "#007BFF",
+          "#D4DEF3",
+          "#333F4F"
+        ]);
+
+      let chart = new CanvasJS.Chart('chartContainer1',
+        {
+          theme: 'light2',
+          animationEnabled: true,
+          exportEnabled: true,
+          colorSet: "greenShades",
+          title: {
+            text: 'Reporte laboral',
+          },
+          data: [{
+            type: 'pie',
+            showInLegend: false,
+            toolTipContent: '<b>{name}</b>: {y} (#percent%)',
+            indexLabel: '{name} - #percent%',
+            dataPoints: [
+            ],
+          }],
+        });
       // Set values on the canva
       for (let i = 0; i < result.length; i++) {
         console.log(result);
-        chart.options.data[0].dataPoints.push({ y: result[i][2], name: result[i][1]});
+        chart.options.data[0].dataPoints.push({ y: result[i][2], name: result[i][1] });
       }
 
       chart.render();
